e4a938bedf8ed519395dc76caa1e00956ba1c93d
sanryuu03 <ryuuosku@gmail.com>
2023-02-25 14:03:54 +0700

    npm-check-updates

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Release Notes
+
+## v1.0.1 (2023-02-25)
+
+### Fixed
+
+-   update package.json (update versi)
+-   https://www.npmjs.com/package/npm-check-updates
+-   npm i npm-check-updates
+-   Or run with npx:
+-   npx npm-check-updates
+-   npx npm-check-updates -u
+-   npm install
+
+## v1.0.0 (2022-11-09)
+
+learn react js v18.2.0.
--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,57 @@
-import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from '@tabler/icons';
+import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from '@tabler/icons-react';
 import clsx from 'clsx';
 import Card from './components/Card.jsx';
+import PlaceContentCenter from './components/PlaceContentCenter.jsx';
 
-const App = () => {
+function App() {
     return (
-        <div className={'bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'}>
-            <div className='max-w-md w-full'>
-                <Card>
-                    <Card.Title>Hello Selir</Card.Title>
-                    <Card.Body>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime omnis hic ducimus. Ullam, harum molestias? Sequi minus, nemo
-                        ex vel modi, eaque molestias saepe eligendi facere non eos autem consequuntur.
-                    </Card.Body>
-                    <Card.Footer>
-                        <div className={'flex gap-x-2'}>
-                            <Button onClick={() => console.log('register')} text='Login' type='submit' />
-                            <Button onClick={() => console.log('register')} text={<IconBrandFacebook />} type='submit' />
-                            <Button className='bg-sky-600'>
-                                <IconBrandTwitter className='w-5 h-5 stroke-1' />
-                                Login
-                            </Button>
-                            <Button
-                                {...{
-                                    className: 'bg-black',
-                                    type: 'submit',
-                                    onClick: () => console.log('login with another style'),
-                                }}>
-                                <IconBrandGithub />
-                                Login
-                            </Button>
-                        </div>
-                    </Card.Footer>
-                </Card>
-            </div>
-        </div>
-    );
-};
+        <PlaceContentCenter>
+            <div className='bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'>
+                <div className='max-w-md w-full'>
+                    <Card>
+                        <Card.Title>Hello Selir</Card.Title>
 
-const Button = (props) => {
+                        <Card.Body>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime omnis hic ducimus. Ullam, harum molestias? Sequi minus,
+                            nemo ex vel modi, eaque molestias saepe eligendi facere non eos autem consequuntur.
+                        </Card.Body>
+
+                        <Card.Footer>
+                            <div className='flex gap-x-2'>
+                                <Button onClick={() => console.log('register')} text='Login' type='submit' />
+
+                                <Button onClick={() => console.log('register')} text={<IconBrandFacebook />} type='submit' />
+
+                                <Button className='bg-sky-600'>
+                                    <IconBrandTwitter className='w-5 h-5 stroke-1' />
+                                    Login
+                                </Button>
+
+                                <Button
+                                    {...{
+                                        className: 'bg-black',
+                                        type: 'submit',
+                                        onClick: () => console.log('login with another style'),
+                                    }}>
+                                    <IconBrandGithub />
+                                    Login
+                                </Button>
+                            </div>
+                        </Card.Footer>
+                    </Card>
+                </div>
+            </div>
+        </PlaceContentCenter>
+    );
+}
+
+function Button(props) {
     const { className = 'bg-blue-600', children, text } = props;
     return (
         <button {...props} className={clsx(className, 'flex items-center gap-x-2  text-white px-4 py-2 rounded')}>
             {text || children}
         </button>
     );
-};
+}
 
 export default App;
